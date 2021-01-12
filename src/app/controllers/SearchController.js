092c8a77ff4c405822d768a20364e648eb02300a
Marcos Carvalho <63380367+MVGC1989@users.noreply.github.com>
2021-01-12 13:55:00 -0300

    SQL página SEARCH

--- src/app/controllers/SearchController.js
+++ src/app/controllers/SearchController.js
@@ -5,30 +5,25 @@ const File = require('../models/File')
 
 module.exports = {
     async index(req , res){
-        let results = await Product.all()
-        const products = results.rows
+        try{
+            let results,
+            params ={}
 
-        if(!products){
-            return res.send("PRODUTO NÃO CADASTRADO!")
+            const {filter , category} = req.query
+
+            if(!filter) return res.redirect("/")
+
+            params.filter = filter
+
+            if(category){
+                params.category = category
+            }
+
+            results = await Product.search(params)
+            
+            return res.render("search/index", {products: lastAdded})
+        }catch(err){
+            consolse.error(err)
         }
-
-        async function getImage(productId){
-            let results = await Product.files(productId)
-            const files = results.rows.map( file => {
-                `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}` 
-            })
-            return files[0]
-        }
-
-        const productsPromise = products.map(async product => {
-            product.image = await getImage(product.id)
-            product.oldPrice = formatPrice(product.old_price)
-            product.price = formatPrice(product.price)
-            return product
-        }).filter((product , index)=> index > 2 ? false : true)
-
-        const lastAdded = await Promise.all(productsPromise)
-
-        return res.render("search/index", {products: lastAdded})
-    }
+    } 
 }
