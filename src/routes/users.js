c46512d8bd7b8cea7678a46c7744bbe674cfce6a
Marcos Carvalho <63380367+MVGC1989@users.noreply.github.com>
2021-01-19 18:55:15 -0300

    Sistema de login

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,13 +4,15 @@ const routes = express.Router()
 const SessionController = require("../app/controllers/SessionController")
 const UserController = require("../app/controllers/UserController")
 
-const Validator = require("../app/validators/user")
+const UserValidator = require("../app/Validators/user")
+const SessionValidator = require("../app/Validators/session")
+
 
 //login/logout
 
-//routes.get('/login', SessionController.loginForm)
-//routes.post('/login', SessionController.login)
-//routes.post('/logout', SessionController.logout)
+routes.get('/login', SessionController.loginForm)
+routes.post('/login', SessionValidator.login, SessionController.login)
+routes.post('/logout', SessionController.logout)
 
 //reset password /forgot
 //routes.get('/forgot-password', SessionController.forgotForm)
@@ -20,9 +22,9 @@ const Validator = require("../app/validators/user")
 
 //user register usercontroller
 routes.get('/register', UserController.registerForm)
-routes.post('/register', Validator.post, UserController.post)
-routes.get('/', Validator.show, UserController.show)
-routes.put('/', Validator.update, UserController.update)
+routes.post('/register', UserValidator.post, UserController.post)
+routes.get('/', UserValidator.show, UserController.show)
+routes.put('/', UserValidator.update, UserController.update)
 //routes.delete('/', UserController.delete)
 
 module.exports = routes 
